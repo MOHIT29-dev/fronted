1e92ef6f9e9689d7e523fbb1c860e23a735b4ebc
MOHIT29-dev <119504966+MOHIT29-dev@users.noreply.github.com>
2023-06-10 20:52:27 +0530

    Add files via upload

---
+++ src/Components/Main.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <div className="main">
+            <div className="col col1">
+                <h2>Navigate borrowing With<br/>Confidence</h2>
+                <br /><br />
+                <p>Built-in analytics to track your finance<br /><br />Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis similique, fuga, ea error natus consequuntur dolorum, perspiciatis sit optio voluptatem aspernatur porro commodi omnis veniam fugiat unde mollitia quibusdam aliquam.</p>
+                <br /><br />
+                <div className="btn_div">
+                <button type="button">Get Started-&gt;</button> 
+                <button type="button">Get Started-&gt;</button>
+                </div>
+            </div>
+            
+            <div className="col">
+                <div className="card card1"></div>
+            </div>
+        </div>
+    );
+}
+export default Main;
